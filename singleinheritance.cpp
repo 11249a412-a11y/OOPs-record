5ba59cad8e0374f16f5a4bb4f8c97bee346b7f94
Y VENKATA SAI KRISHNA <11249a412@kanchiuniv.ac.in>
2025-11-05 21:43:06 -0800

    Add files via upload

---
+++ singleinheritance.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+
+class Animal {
+public:
+    void eat() {
+        cout << "This animal can eat" << endl;
+    }
+};
+
+
+class Dog : public Animal {
+public:
+    void bark() {
+        cout << "The dog is barking" << endl;
+    }
+};
+
+int main() {
+    
+    Dog myDog;
+
+    
+    myDog.eat();  
+
+    
+    myDog.bark();  
+
+    return 0;
+}
